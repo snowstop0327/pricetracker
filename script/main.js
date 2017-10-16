f0893cf35389a4fa98b6e4169e7545b7a0ea3301
snow_stop <snow.huang@ezprice.com.tw>
2017-10-16 10:06:43 +0800

    first-update

---
+++ script/main.js
@@ -0,0 +1,27 @@
+
+
+//@prepros-append vendors/popper.min.js
+
+// bootstrap js
+//@prepros-append bootstrap.min.js
+
+// templete ui
+//@prepros-append pace.min.js
+//@prepros-append app.js
+
+// material ui
+//@prepros-append material.js
+//@prepros-append vendors/ripples.js
+
+// include Bootstrap time Picker
+//@prepros-append vendors/moment.min.js
+//@prepros-append vendors/daterangepicker.js
+
+// include slimscroll
+//@prepros-append vendors/jquery.slimscroll.js
+
+// include stupidtable
+//@prepros-append vendors/stupidtable.js
+
+
+// my js
