271d41d2b607fb58bd4ccbf82f489f7f4a0d4f3f
snowstop19900327 <hst-790327@hotmail.com>
2017-10-23 12:03:53 +0800

    pricetracker_intro_v1_OK

---
+++ PriceTracker_Intro/script/main.js
@@ -0,0 +1,9 @@
+// jquery include
+// @prepros-append jquery-slim.min.js
+
+// material ui
+//@prepros-append material.js
+//vendors/ripples.js
+
+// sticky-header
+//@prepros-append pricetracker.js
---
+++ PriceTracker_Intro/script/pricetracker.js
@@ -0,0 +1,18 @@
+// sticky-header
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 80) {
+       $('.header-fixed').addClass('is-sticky');
+       // $('.sticky-area').addClass('container');
+    }
+    else {
+       $('.header-fixed').removeClass('is-sticky');
+       // $('.sticky-area').removeClass('container');
+    }
+});
+
+// navbar-toggle-button
+
+$('.navbar-toggler').click(function(event) {
+	/* Act on the event */
+	$('.navbar-end').toggleClass('open');
+});
