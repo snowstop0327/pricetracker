1e0b9117ad465875eb9e90d814d32e6839d4509c
snow_stop <snow.huang@ezprice.com.tw>
2017-12-07 15:55:04 +0800

    通路端初階完成

---
+++ script/front-page/front.js
@@ -0,0 +1,12 @@
+// jquery include
+// @prepros-append jquery-slim.min.js
+
+// material ui
+//@prepros-append material.js
+
+// material pace
+//@prepros-append pace.js
+
+// sticky-header
+//@prepros-append pricetracker.js
+
---
+++ script/front-page/pricetracker.js
@@ -0,0 +1,26 @@
+// jquery include
+// @prepros-append jquery-slim.min.js
+
+// material ui
+//@prepros-append material.js
+//vendors/ripples.js
+
+// sticky-header
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 80) {
+    $('.header-fixed').addClass('is-sticky');
+    // $('.sticky-area').addClass('container');
+  }
+  else {
+    $('.header-fixed').removeClass('is-sticky');
+    // $('.sticky-area').removeClass('container');
+  }
+});
+
+// navbar-toggle-button
+
+$('.navbar-toggler').click(function (event) {
+  /* Act on the event */
+  $(this).toggleClass('icon-android-close');
+  $('.navbar-end').toggleClass('open');
+});
